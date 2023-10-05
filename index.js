d537b0424e664e44573e47394f619a0ee6ae5957
mrakhaf <muhammadrakhaf@gmail.com>
2023-10-05 20:17:17 +0700

    add: test

---
+++ index.js
@@ -0,0 +1,18 @@
+class Human {
+    static isLivingonDeath = true;
+
+    constructor(name, address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    introduce(){
+        console.log(`Hi, my name is ${this.name}`)
+    }
+}
+
+let person = new Human("rakha", "binong");
+
+person.
+
+console.log(person.introduce());
